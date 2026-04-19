ee338b5fb86e59a2243be7c9e07d8a6b32f160db
SuperDudePro <william.gayhart@gmail.com>
2026-04-19 15:59:24 -0600

    update 1

---
+++ src/AQR.tsx
@@ -0,0 +1,18 @@
+import "./AQR.css";
+
+function AQR() {
+  return (
+    <main className="page">
+      <section
+        className="hero"
+        aria-label="Applied Quantitative Reasoning launch page"
+      >
+        <h1 className="mark">AQR</h1>
+        <p className="full-name">Applied Quantitative Reasoning</p>
+        <p className="subhead">Coming Fall Semester 2026</p>
+      </section>
+    </main>
+  );
+}
+
+export default AQR;
