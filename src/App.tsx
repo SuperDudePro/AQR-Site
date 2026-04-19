0511a0db3a98b5a05ddf77b493e9bd93dbe7387d
SuperDudePro <william.gayhart@gmail.com>
2026-04-19 15:26:10 -0600

    initial upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="page">
+      <section
+        className="hero"
+        aria-label="Applied Quantitative Reasoning launch page"
+      >
+        <h1 className="mark">AQR</h1>
+        <p className="subhead">Coming Fall Semester 2026</p>
+      </section>
+    </main>
+  );
+}
+
+export default App;
