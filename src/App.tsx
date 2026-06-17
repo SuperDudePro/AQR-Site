5a5f35dba0d251bb82c69e8b5d3b098b7b72ed9b
SuperDudePro <william.gayhart@gmail.com>
2026-06-16 21:08:25 -0600

    Use head-loaded Google tag

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,12 @@ function ensureGoogleAnalytics() {
     return;
   }
 
-  if (!document.querySelector(`script[src="https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}"]`)) {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
-    document.head.appendChild(script);
-  }
-
   window.dataLayer = window.dataLayer || [];
-  window.gtag = function gtag(...args: unknown[]) {
-    window.dataLayer?.push(args);
-  };
+  window.gtag =
+    window.gtag ||
+    function gtag(...args: unknown[]) {
+      window.dataLayer?.push(args);
+    };
 
   window.gtag("js", new Date());
   window.gtag("config", GA_TRACKING_ID, {
